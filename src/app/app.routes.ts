50559ca8a2fdf0ce75230e5c4d6fa8520b945192
THAWA LUKA PEREIRA GONCALVES DOS SANTOS <thawa.lpgsantos@senacsp.edu.br>
2026-09-22 22:22:00 -0300

    Tela de ajuda parcial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { AjudaComponent } from './pages/ajuda/ajuda.component';
 
 export const routes: Routes = [
+    { path: 'ajuda', component: AjudaComponent },
     { path: '', component: HomeComponent},
 ];
