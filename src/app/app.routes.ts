7656b35ca58e934d1f219e6c27ef471665b0f9da
MATHEUS REIS FABIANO <matheus.rfabiano@senacsp.edu.br>
2026-09-22 22:18:22 -0300

    Tela contato parcial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { ContatoComponent } from './pages/contato/contato.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent},
+    { path: 'contato', component: ContatoComponent},
+    { path: '', component: HomeComponent}
 ];
